e62b15725dc94da5793e353b91313dbf41cd1536
Bhethalam,Lohitha <s533975@ol10020m.nwmissouri.edu>
2019-02-14 14:07:41 -0600

    Initial Commit

---
+++ The ArithMETic App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The ArithMETic App
+//
+//  Created by Bhethalam,Lohitha on 2/14/19.
+//  Copyright © 2019 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
